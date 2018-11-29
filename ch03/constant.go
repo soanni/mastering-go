5cd6afaf635aab39b8417fa1a9f740343fdafe3a
soanni <andrey.solodov@aurea.com>
2018-11-29 20:54:37 +0400

    added map, constants examples

---
+++ ch03/constant.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+const s1 = 123
+const s2 float64 = 123
+
+func main() {
+	var v1 float32 = s1 * 12
+	var v2 float32 = s2 * 12
+}
--- ch03/sortSlice.go
+++ ch03/sortSlice.go
@@ -20,4 +20,14 @@ func main() {
 	mySlice = append(mySlice, aStructure{"Athina", 134, 40})
 
 	fmt.Println("0:", mySlice)
+
+	sort.Slice(mySlice, func(i, j int) bool {
+		return mySlice[i].height < mySlice[j].height
+	})
+	fmt.Println("<:", mySlice)
+
+	sort.Slice(mySlice, func(i, j int) bool {
+		return mySlice[i].height > mySlice[j].height
+	})
+	fmt.Println(">:", mySlice)
 }
